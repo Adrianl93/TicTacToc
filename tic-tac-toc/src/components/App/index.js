041b8bcb120527bc546440b3b12eb83564f434b1
Adrianl93 <laborde.adrian2012@gmail.com>
2023-05-20 12:34:03 -0300

    cells created

---
+++ tic-tac-toc/src/components/App/index.js
@@ -0,0 +1,11 @@
+import { Game } from '../Game'
+const App=() => {
+    return(
+        <>
+            <h1>Hi Bitch </h1>
+            <Game />
+        </>
+    )
+
+}
+export {App};
---
+++ tic-tac-toc/src/components/Game/index.js
@@ -0,0 +1,28 @@
+import './Game.css'
+
+const Cell = ()=>{
+    return(
+        <div className="Game-cell"></div>
+    )
+}
+
+
+const Game=() => {
+    return(
+        <section className="Game">
+            
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+
+        </section>
+    )
+
+}
+export {Game};
