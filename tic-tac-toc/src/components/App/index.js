eac66aa0b033756a5ef81b4eb4e0b608a4d99bc7
Adrianl93 <laborde.adrian2012@gmail.com>
2023-05-21 12:06:25 -0300

    state changes enabled

--- tic-tac-toc/src/components/App/index.js
+++ tic-tac-toc/src/components/App/index.js
@@ -1,9 +1,30 @@
 import { Game } from '../Game'
+import { useState } from 'react';
 const App=() => {
+    const[player, setPlayer] = useState('X')
+    const[table, setTable] = useState(
+        [
+            null, null, null, 
+            null, null, null,
+            null, null, null
+        ]
+    )
+
+    const onClickCell = (index)=>{
+        if(table[index] === null){
+        const newTable = table.slice()
+        newTable[index] = player
+        setTable(newTable)
+        player === 'X'
+        ? setPlayer('0')
+        : setPlayer('X')
+    }
+};
+
     return(
         <>
-            <h1>Hi Bitch </h1>
-            <Game />
+            <h1>Tic Tac Toc - Turn of: {player} </h1>
+            <Game table={table} player={player} onClickCell={onClickCell}/>
         </>
     )
 
--- tic-tac-toc/src/components/Game/index.js
+++ tic-tac-toc/src/components/Game/index.js
@@ -1,25 +1,28 @@
-import './Game.css'
 
-const Cell = ()=>{
+import './Game.css';
+
+const Cell = ({onClickCell, table, indexTable}) => {
+
+
     return(
-        <div className="Game-cell"></div>
+        <div onClick={() => onClickCell(indexTable) } className="Game-cell">{table[indexTable]}</div>
     )
 }
 
 
-const Game=() => {
+const Game=({table, onClickCell}) => {
     return(
         <section className="Game">
             
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={0}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={1}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={2}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={3}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={4}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={5}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={6}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={7}/>
+            <Cell table={table} onClickCell={onClickCell} indexTable={8}/>
 
         </section>
     )
